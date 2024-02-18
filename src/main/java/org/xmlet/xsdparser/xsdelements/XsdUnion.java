29ea008e71e72e5113c3b4cd152aae00321adc47
Daniel Radtke <DanielRadtke@hotmail.de>
2024-02-18 16:40:39 +0100

    reformat

--- src/main/java/org/xmlet/xsdparser/xsdelements/XsdUnion.java
+++ src/main/java/org/xmlet/xsdparser/xsdelements/XsdUnion.java
@@ -1,17 +1,18 @@
 package org.xmlet.xsdparser.xsdelements;
 
+import org.xmlet.xsdparser.core.XsdParserCore;
+import org.xmlet.xsdparser.core.utils.ParseData;
+import org.xmlet.xsdparser.xsdelements.elementswrapper.ReferenceBase;
+import org.xmlet.xsdparser.xsdelements.elementswrapper.UnsolvedReference;
+import org.xmlet.xsdparser.xsdelements.visitors.XsdAbstractElementVisitor;
+
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import javax.validation.constraints.NotNull;
-import org.xmlet.xsdparser.core.XsdParserCore;
-import org.xmlet.xsdparser.core.utils.ParseData;
-import org.xmlet.xsdparser.xsdelements.elementswrapper.ReferenceBase;
-import org.xmlet.xsdparser.xsdelements.elementswrapper.UnsolvedReference;
-import org.xmlet.xsdparser.xsdelements.visitors.XsdAbstractElementVisitor;
 
 /**
  * A class representing the xsd:union element.
@@ -46,20 +47,19 @@ public class XsdUnion extends XsdAnnotatedElements {
         visitorParam.visit(this);
     }
 
-  /**
-   * Performs a copy of the current object for replacing purposes. The cloned objects are used to
-   * replace {@link UnsolvedReference} objects in the reference solving process.
-   *
-   * @param placeHolderAttributes The additional attributes to add to the clone.
-   * @return A copy of the object from which is called upon.
-   */
-  @Override
-  public XsdUnion clone(@NotNull Map<String, String> placeHolderAttributes) {
+    /**
+     * Performs a copy of the current object for replacing purposes. The cloned objects are used to replace
+     * {@link UnsolvedReference} objects in the reference solving process.
+     * @param placeHolderAttributes The additional attributes to add to the clone.
+     * @return A copy of the object from which is called upon.
+     */
+    @Override
+    public XsdUnion clone(@NotNull Map<String, String> placeHolderAttributes) {
         placeHolderAttributes.putAll(attributesMap);
 
         XsdUnion elementCopy = new XsdUnion(this.parser, placeHolderAttributes, visitorFunction);
 
-    if (this.simpleTypeList != null) {
+        if (this.simpleTypeList != null){
             elementCopy.simpleTypeList = this.simpleTypeList.stream().map(simpleType -> (XsdSimpleType) simpleType.clone(simpleType.getAttributesMap(), elementCopy)).collect(Collectors.toList());
         }
 
@@ -68,7 +68,7 @@ public class XsdUnion extends XsdAnnotatedElements {
         return elementCopy;
     }
 
-  public List<XsdSimpleType> getUnionElements() {
+    public List<XsdSimpleType> getUnionElements(){
         return simpleTypeList;
     }
 
@@ -95,8 +95,7 @@ public class XsdUnion extends XsdAnnotatedElements {
     }
     return unsolvedMemberTypes;
   }
-
-  public static ReferenceBase parse(@NotNull ParseData parseData) {
+    public static ReferenceBase parse(@NotNull  ParseData parseData){
         return xsdParseSkeleton(parseData.node, new XsdUnion(parseData.parserInstance, convertNodeMap(parseData.node.getAttributes()), parseData.visitorFunction));
     }
 
